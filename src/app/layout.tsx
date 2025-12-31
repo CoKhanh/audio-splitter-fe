317d0e60fb36557dcb92b1cebce72cd6bac12184
duongcokhanh <khanh@story.co>
2025-12-31 18:55:15 +0700

    Add GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { GoogleTagManager } from "@next/third-parties/google"
+import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <GoogleTagManager gtmId="GTM-W3W43SXW" />
+      <GoogleAnalytics gaId="G-8EY9SPGG0D" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
